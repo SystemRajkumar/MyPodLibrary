2f664c1b3c040526de2c834d93c3e3e093034970
Rajkumar Kumawat <rajkumar.kumawat@truworth.com>
2023-02-02 14:53:59 +0530

    changes

--- Example/MyPodLibrary/ViewController.swift
+++ Example/MyPodLibrary/ViewController.swift
@@ -20,8 +20,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    }
 }
 
